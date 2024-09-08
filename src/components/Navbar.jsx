2c6b2e6049e0d4a9e573257bdf16d5608d021045
StevenTB1 <stevenchow0812@gmail.com>
2024-09-07 23:53:41 -0400

    9.7 Setup and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className='flex flex-shrink-0 items-center'>
+            <img src="" alt=""></img>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
