28135c403d7cefac66ab4fdf65a1d16535232e1b
steve <s9chow@uwaterloo.ca>
2024-09-13 09:37:35 -0400

    creating experience page

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div className='border-b-2 border-neutral-500 pb-20'>
         <h1 className='bg-gradient-to-r from-blue-300 via-indigo-400 to-purple-200 bg-clip-text text-transparent my-10 text-center text-4xl'>Contact Me</h1>
         <div className='text-center tracking-tighter'>
-            <p className='mb-4'>+1 (437)213-3671</p>
+            <p className='mb-4'>+1 (437) 213-3671</p>
             <a href="#" className='border-b'>
                 s9chow@uwaterloo.ca
             </a>
